d0e81fe8ca473d4d247220928224013468c422c9
ERIC DE LUCAS SILVA <126726641+Ericdelucas@users.noreply.github.com>
2025-10-22 11:38:22 -0300

    Criação da correção

---
+++ trabalho/coordenadas.py
@@ -0,0 +1,14 @@
+import pyautogui
+import time
+
+# time.sleep(3)
+# x=pyautogui.position()
+# print(x)
+
+
+
+
+time.sleep(9)
+pyautogui.click(x=1334, y=92)
+time.sleep(0.3)
+pyautogui.click(x=1330, y=415)
